df770637cb105eb43507f468aa40d27a4a2ed77b
Jim Balcomb <jim.balcomb@gmail.com>
2018-11-08 19:00:21 +0000

    added config files and shell script to copy config files

---
+++ webapp-python/copy_setup.sh
@@ -0,0 +1,10 @@
+
+cp /etc/apache2/apache2.conf ~/development/webapp-python/webapp-python/_ubuntu_server/etc/apache2/
+cp /etc/apache2/ports.conf ~/development/webapp-python/webapp-python/_ubuntu_server/etc/apache2/
+
+cp /etc/apache2/mods-enabled/wsgi.conf ~/development/webapp-python/webapp-python/_ubuntu_server/etc/apache2/mods-enabled/
+cp /etc/apache2/mods-enabled/wsgi.load ~/development/webapp-python/webapp-python/_ubuntu_server/etc/apache2/mods-enabled/
+
+cp /etc/apache2/sites-enabled/boringbalcomb.com.conf ~/development/webapp-python/webapp-python/_ubuntu_server/etc/apache2/sites-enabled/
+#cp /etc/apache2/sites-enabled/boringbalcomb.com-ssl.conf ~/development/webapp-python/webapp-python/_ubuntu_server/etc/apache2/sites-enabled/
+
