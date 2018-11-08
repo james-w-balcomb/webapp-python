93ef25f10b1627c3c75db4b4e08d696d4777bff7
Jim Balcomb <jim.balcomb@gmail.com>
2018-11-08 18:55:10 +0000

    created shell scripts for develop and deploy

---
+++ webapp-python/copy_webapp.sh
@@ -0,0 +1,5 @@
+
+cp /var/www/python/boringbalcomb.com/webapp-python/webapp_python.wsgi ~/development/webapp-python/webapp-python/webapp_python/
+
+cp /var/www/python/boringbalcomb.com/webapp-python/webapp_python.py ~/development/webapp-python/webapp-python/webapp_python/
+
---
+++ webapp-python/deploy_webapp.sh
@@ -0,0 +1,6 @@
+
+cp ~/development/webapp-python/webapp-python/webapp_python/webapp_python.wsgi /var/www/python/boringbalcomb.com/webapp-python/
+cp ~/development/webapp-python/webapp-python/webapp_python/webapp_python.py /var/www/python/boringbalcomb.com/webapp-python/
+
+touch /var/www/python/boringbalcomb.com/webapp-python/webapp_python.wsgi
+
