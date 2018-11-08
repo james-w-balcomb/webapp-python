4a6f5f89d8c00e3cbbcda34fa4b81c94dc4015b4
Jim Balcomb <jim.balcomb@gmail.com>
2018-11-08 22:32:34 +0000

    refactored to use flask

--- webapp-python-flask/webapp_python_flask/webapp_python_flask.py
+++ webapp-python-flask/webapp_python_flask/webapp_python_flask.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+webapp_python_flask = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@webapp_python_flask.route("/")
+def wsgi_script_alias_root():
     return "Hello World"
